7f13139ca4a4c58b4c9c6e048f689af7f761a0fc
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-06-04 20:16:42 -0500

    made a basic header for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './header.js'
+
 function App() {
   return (
     <div className="App">
+      <Header />
       hello world
     </div>
   );
---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+    return (
+      <section className="headerBar">
+        <h1>
+            Burger Builder App
+        </h1>
+      </section>
+    );
+  }
+  
+  export default Header;
+  
