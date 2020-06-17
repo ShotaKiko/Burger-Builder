dfa7097e9eab906cf79ac5101baf92a599deefe6
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-06-16 19:46:47 -0500

    added pizza section render on button click

--- src/PizzaParlors/PizzaJoints.js
+++ src/PizzaParlors/PizzaJoints.js
@@ -9,18 +9,37 @@ class PizzaJoints extends React.Component{
             {id:"pizza2", name:"Dominos", motto:"It's what we do."},
             {id:"pizza3", name:"Cici's", motto:"Pizza Pizza."}
         ],
-        showPizzerias: false
+        showPizza: false
     }
 
+    clickHandler = () => {
+        const showComponent = this.state.showPizza
+        this.setState({
+            showPizza: !showComponent
+        })
+    }
+    
     render(){
+        let pizzaSection = null;
+        
+
+        if (this.state.showPizza) {
+            pizzaSection = (
+                <div className="pizzasRendered">
+                    
+                    {this.state.pizzaParlors.map((pizzeria, id) => {
+                        return <Pizzeria 
+                            name={pizzeria.name}
+                            motto={pizzeria.motto}
+                        />
+                    })} 
+                </div>
+            )
+        }
         return(
             <div className="pizzeriaContainer">
-                {this.state.pizzaParlors.map((pizzeria, id) => {
-                    return <Pizzeria 
-                        name={pizzeria.name}
-                        motto={pizzeria.motto}
-                    />
-                })} 
+                <button onClick={this.clickHandler}>See the pizza</button>
+                {pizzaSection}
             </div>
         )
     }
